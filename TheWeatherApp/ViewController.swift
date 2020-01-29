ace84cd11930091688e967fa7a21111081d7cd07
moremist <rbest15@rambler.ru>
2020-01-29 18:27:07 +0300

    Initial Commit

---
+++ TheWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheWeatherApp
+//
+//  Created by Roman Kuzmich on 29.01.2020.
+//  Copyright © 2020 Roman Kuzmich Home Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
